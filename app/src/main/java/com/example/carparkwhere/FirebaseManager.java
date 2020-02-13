fec53d0e0e53d9b366814b15208a2a1728447414
sweesenkoh <sweesenworkstation@gmail.com>
2020-02-13 23:18:21 +0800

    done with linking with firebase

---
+++ app/src/main/java/com/example/carparkwhere/FirebaseManager.java
@@ -0,0 +1,102 @@
+package com.example.carparkwhere;
+
+import android.app.Activity;
+import android.content.Context;
+
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class FirebaseManager {
+
+    private static FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private static FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    public enum CollectionsName
+    {
+        USERS("users"),
+        FAVOURITE_CARPARKS("favouriteCarparks"),
+        CARPARKREVIEWS("carparkReviews");
+
+        private String collectionName;
+
+        CollectionsName(String collectionName) {
+            this.collectionName = collectionName;
+        }
+
+        public String getCollectionName() {
+            return this.collectionName;
+        }
+    }
+
+
+
+    public static FirebaseUser getCurrentUser(){
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        return currentUser;
+    }
+
+    public static void createNewUser(Context context, final String email, String password, OnCompleteListener<AuthResult> handler){
+        mAuth.createUserWithEmailAndPassword(email, password)
+        .addOnCompleteListener((Activity) context, handler)
+        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Map<String,Object> user = new HashMap<>();
+                user.put("displayName",email);
+                user.put("email",email);
+                FirebaseManager.insertToFirestore(CollectionsName.USERS.getCollectionName(), getCurrentUser().getUid(), user, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+
+                    }
+                });
+
+            }
+        });
+    }
+
+    public static void signInWithEmail(Context context, String email, String password, OnCompleteListener<AuthResult> handler){
+        mAuth.signInWithEmailAndPassword(email, password)
+        .addOnCompleteListener((Activity) context,handler);
+    }
+
+
+
+
+
+
+
+    public static void insertToFirestore(String collectionName, String documentName, Map<String,Object> data, OnCompleteListener<Void> handler){
+        db.collection(collectionName).document(documentName)
+                .set(data)
+                .addOnCompleteListener(handler);
+    }
+
+    public static void updateFieldFirestore(String collectionName, String documentName, Map<String,Object> data){
+        db.collection(collectionName).document(documentName).set(data,SetOptions.merge());
+    }
+
+    public static void retrieveFromFirestore(String collectionName, String documentName, OnCompleteListener<DocumentSnapshot> handler) {
+        DocumentReference docRef = db.collection(collectionName).document(documentName);
+        docRef.get().addOnCompleteListener(handler);
+    }
+
+}
---
+++ app/src/main/java/com/example/carparkwhere/UserDataManager.java
@@ -0,0 +1,57 @@
+package com.example.carparkwhere;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.firestore.DocumentSnapshot;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserDataManager {
+
+    public static String getUserEmail(){
+        return FirebaseManager.getCurrentUser().getEmail();
+    }
+
+    public static String getDisplayName(){
+        return FirebaseManager.getCurrentUser().getDisplayName();
+    }
+
+    public static void updateDisplayName(String displayName){
+        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder().setDisplayName(displayName).build();
+        FirebaseManager.getCurrentUser().updateProfile(profileUpdates);
+        Map<String,Object> displayNameMap = new HashMap<>();
+        displayNameMap.put("displayName",displayName);
+        FirebaseManager.updateFieldFirestore("users",FirebaseManager.getCurrentUser().getUid(),displayNameMap);
+    }
+
+    public static void addNewFavouriteCarpark(final String carparkId){
+        FirebaseManager.retrieveFromFirestore(FirebaseManager.CollectionsName.FAVOURITE_CARPARKS.getCollectionName(), FirebaseManager.getCurrentUser().getUid(), new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                Map<String,Object> carparkMap = new HashMap<>();
+                ArrayList<String> carparks = new ArrayList<>();
+                carparks = (ArrayList<String>) task.getResult().get("carparks");
+                if (!carparks.contains(carparkId)){carparks.add(carparkId);}
+                carparkMap.put("carparks",carparks);
+                FirebaseManager.updateFieldFirestore(FirebaseManager.CollectionsName.FAVOURITE_CARPARKS.getCollectionName(),FirebaseManager.getCurrentUser().getUid(),carparkMap);
+            }
+        });
+    }
+
+    public static void addNewReview(String carparkID, Double rating,String review){
+        Map<String,Object> reviewsMap = new HashMap<>();
+        ArrayList<Map<String,Object>> reviews = new ArrayList<>();
+        Map<String,Object> reviewMap = new HashMap<>();
+        reviewMap.put("userId",FirebaseManager.getCurrentUser().getUid());
+        reviewMap.put("rating",rating);
+        reviewMap.put("review",review);
+        reviews.add(reviewMap);
+        reviewsMap.put("reviews",reviews);
+
+        FirebaseManager.updateFieldFirestore(FirebaseManager.CollectionsName.CARPARKREVIEWS.getCollectionName(),carparkID,reviewsMap);
+    }
+}
