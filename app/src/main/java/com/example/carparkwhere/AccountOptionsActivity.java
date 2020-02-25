848978f3cb09150812d89cad9679b5fe3064b3ac
tjaslyn <31032040+tjaslyn@users.noreply.github.com>
2020-02-25 11:36:49 +0800

    Added blank onClickListeners

--- app/src/main/java/com/example/carparkwhere/AccountOptionsActivity.java
+++ app/src/main/java/com/example/carparkwhere/AccountOptionsActivity.java
@@ -3,6 +3,8 @@ package com.example.carparkwhere;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AccountOptionsActivity extends AppCompatActivity {
 
@@ -10,5 +12,46 @@ public class AccountOptionsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_options);
+
+        Button viewUserBookmarks_BTN = findViewById(R.id.viewUserBookmarks_BTN);
+        viewUserBookmarks_BTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        Button viewUserReviews_BTN = findViewById(R.id.viewUserReviews_BTN);
+        viewUserReviews_BTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        Button signOutAccount_BTN = findViewById(R.id.signOutAccount_BTN);
+        signOutAccount_BTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        Button viewResetPassword_BTN = findViewById(R.id.viewResetPassword_BTN);
+        viewResetPassword_BTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        Button viewDeactivateAccount_BTN = findViewById(R.id.viewDeactivateAccount_BTN);
+        viewDeactivateAccount_BTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
