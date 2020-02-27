facb8a5e7a3942e973efa3fa025cd770438fcb3a
tjaslyn <31032040+tjaslyn@users.noreply.github.com>
2020-02-27 16:23:17 +0800

    Skeleton for Submit Review Activity

---
+++ app/src/main/java/com/example/carparkwhere/SubmitReviewActivity.java
@@ -0,0 +1,14 @@
+package com.example.carparkwhere;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SubmitReviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_submit_review);
+    }
+}
