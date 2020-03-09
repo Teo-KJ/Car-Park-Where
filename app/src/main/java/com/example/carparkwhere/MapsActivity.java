d4af5e0e916c04f1c2799b639ac91894a6bcdbee
Li Wei-Ting <44592423+leady53@users.noreply.github.com>
2020-03-09 16:45:42 +0800

    Added carpark markers (default marker)

--- app/src/main/java/com/example/carparkwhere/MapsActivity.java
+++ app/src/main/java/com/example/carparkwhere/MapsActivity.java
@@ -4,7 +4,13 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.example.carparkwhere.Models.CarparkJson;
+import com.example.carparkwhere.Utilities.ServerInterfaceManager;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -15,6 +21,11 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
@@ -27,13 +38,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     Location currentLocation;
     FusedLocationProviderClient fusedLocationProviderClient;
     private static final int REQUEST_CODE = 101;
+    ArrayList<CarparkJson> carparks = new ArrayList<CarparkJson>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
         fetchLocation();
+
     }
+
+
     private void fetchLocation() {
         if (ActivityCompat.checkSelfPermission(
                 this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
@@ -55,13 +70,36 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         });
     }
+
+
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(final GoogleMap googleMap) {
+
+        ServerInterfaceManager.getAllCarparkCoordinates(this, new Response.Listener() {
+            @Override
+            public void onResponse(Object response) {
+                Gson gson = new Gson();
+                carparks = gson.fromJson(response.toString(),new TypeToken<ArrayList<CarparkJson>>(){}.getType());
+                //carparks here, set the marker information here
+                for (int counter = 0; counter < carparks.size(); counter++) {
+                    googleMap.addMarker(new MarkerOptions()
+                            .position(new LatLng(carparks.get(counter).latitude, carparks.get(counter).longitude))
+                            .title(carparks.get(counter).carparkName));
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println(error.getMessage());
+            }
+        });
         LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
         MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("Current Location");
+
         googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
         googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 12));
         googleMap.addMarker(markerOptions);
+
     }
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
