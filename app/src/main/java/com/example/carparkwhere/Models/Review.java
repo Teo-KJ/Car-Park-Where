9eee34bfaf62934438b126a0b42bbca0fefac918
tjaslyn <31032040+tjaslyn@users.noreply.github.com>
2020-03-11 22:20:56 +0800

    Created RegisteredUser and Review Entities classes.

---
+++ app/src/main/java/com/example/carparkwhere/Models/Review.java
@@ -0,0 +1,47 @@
+package com.example.carparkwhere.Models;
+
+import java.util.List;
+
+public class Review {
+
+    private RegisteredUser user;
+    private int userRating;
+    private Carpark carpark;
+    private String userComments;
+
+    public Review(RegisteredUser user, int userRating, Carpark carpark, String userComments){
+        this.user = user;
+        this.userRating = userRating;
+        this.carpark = carpark;
+        this.userComments = userComments;
+
+    }
+
+    public void loadReviewData(){
+
+    }
+
+    public void saveReviewData(){
+
+    }
+
+    public List<Review> getCarparkReviews(){
+        List<Review> reviewList = null;
+
+        return reviewList;
+    }
+
+    public void setCarPark(String carparkID){
+        //this.carpark = ...?
+
+    }
+
+    public void setUser(String userEmail){
+
+    }
+
+    public void setRating(int rating){
+        this.userRating = rating;
+    }
+
+}
