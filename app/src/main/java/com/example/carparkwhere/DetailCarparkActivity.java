185a76590ed538cdbb7f15553e25a25be3fbe6f5
Teo-KJ <48685014+Teo-KJ@users.noreply.github.com>
2020-03-20 15:13:52 +0800

    Update DetailCarparkActivity.java

--- app/src/main/java/com/example/carparkwhere/DetailCarparkActivity.java
+++ app/src/main/java/com/example/carparkwhere/DetailCarparkActivity.java
@@ -69,6 +69,10 @@ public class DetailCarparkActivity extends AppCompatActivity {
     private Spinner spinner;
     private ArrayList<String> userBookmarkCarparks;
     private boolean userBookmarkedThis = false;
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
+
 >>>>>>> Stashed changes
 
     @Override
@@ -281,14 +285,16 @@ public class DetailCarparkActivity extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 int difference = i - finalDay;
+                getAvailabilityPredictionData(difference, str);
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
                 int difference = 0;
+                getAvailabilityPredictionData(difference, str);
             }
         });
-
+        //difference
 
         // Bar chart for the visualisation
         barChart = findViewById(R.id.visualisation);
@@ -297,12 +303,43 @@ public class DetailCarparkActivity extends AppCompatActivity {
         barChart.setNoDataText("Loading the data...");
         barChart.getAxisLeft().setDrawLabels(false);
         barChart.getAxisRight().setDrawLabels(false);
+    }
 
+    //   Function for Progress bar to load carpark detail
+    private void presentProgressDialog(String message){
+        nDialog = new ProgressDialog(DetailCarparkActivity.this);
+        nDialog.setMessage("Loading..");
+        nDialog.setTitle(message);
+        nDialog.setIndeterminate(false);
+        nDialog.setCancelable(true);
+        nDialog.show();
+    }
+
+    // Function to identify the day of the week
+    private int identifyDay (){
+        Calendar calendar = Calendar.getInstance(TimeZone.getDefault());
+        return calendar.get(Calendar.DAY_OF_WEEK) % 7-2;
+    }
+
+    // Function to open up the help dialogue
+    public void openDialog() {
+        Dialog help = new Dialog(DetailCarparkActivity.this);
+        help.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        help.setContentView(R.layout.detail_carpark_tutorial);
+        help.show();
+    }
+
+<<<<<<< Updated upstream
         // With ServerInterfaceManager, get the predicted number of carpark lots.
 <<<<<<< Updated upstream
         ServerInterfaceManager.getCarparkWholeDayPredictedAvailability(this, str, new Response.Listener() {
 =======
         ServerInterfaceManager.getCarparkWholeDayPredictedAvailability(this, str, 2, new Response.Listener() {
+>>>>>>> Stashed changes
+=======
+    // With ServerInterfaceManager, get the predicted number of carpark lots.
+    private void getAvailabilityPredictionData(Integer increment, String str){
+        ServerInterfaceManager.getCarparkWholeDayPredictedAvailability(this, str, increment, new Response.Listener() {
 >>>>>>> Stashed changes
             @Override
             public void onResponse(Object response){
@@ -336,31 +373,6 @@ public class DetailCarparkActivity extends AppCompatActivity {
                 error.printStackTrace();
             }
         });
-
-    }
-
-    //   Function for Progress bar to load carpark detail
-    private void presentProgressDialog(String message){
-        nDialog = new ProgressDialog(DetailCarparkActivity.this);
-        nDialog.setMessage("Loading..");
-        nDialog.setTitle(message);
-        nDialog.setIndeterminate(false);
-        nDialog.setCancelable(true);
-        nDialog.show();
-    }
-
-    // Function to identify the day of the week
-    private int identifyDay (){
-        Calendar calendar = Calendar.getInstance(TimeZone.getDefault());
-        return calendar.get(Calendar.DAY_OF_WEEK) % 7-2;
-    }
-
-    // Function to open up the help dialogue
-    public void openDialog() {
-        Dialog help = new Dialog(DetailCarparkActivity.this);
-        help.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        help.setContentView(R.layout.detail_carpark_tutorial);
-        help.show();
     }
 
 }
