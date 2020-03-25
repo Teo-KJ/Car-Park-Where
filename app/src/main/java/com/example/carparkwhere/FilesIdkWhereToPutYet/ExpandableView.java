9998e078134ef700ff0c820f8deccde2d4306561
sweesenkoh <sweesenworkstation@gmail.com>
2020-03-25 09:50:09 +0800

    added connection to user bookmark database

--- app/src/main/java/com/example/carparkwhere/FilesIdkWhereToPutYet/ExpandableView.java
+++ app/src/main/java/com/example/carparkwhere/FilesIdkWhereToPutYet/ExpandableView.java
@@ -5,8 +5,8 @@ import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
-import com.example.carparkwhere.FilesIdkWhereToPutYet.RecyclerAdapter;
-import com.example.carparkwhere.ModelObjects.BookmarkedCarparks;
+
+import com.example.carparkwhere.ModelObjects.BookmarkedCarpark;
 import com.example.carparkwhere.R;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,7 +16,7 @@ public class ExpandableView extends AppCompatActivity {
     RecyclerView recyclerView;
     RecyclerAdapter recyclerAdapter;
 
-    List<BookmarkedCarparks> bookmarkedCarparks;
+    List<BookmarkedCarpark> bookmarkedCarparks;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,9 +30,9 @@ public class ExpandableView extends AppCompatActivity {
         recyclerView.addItemDecoration(dividerItemDecoration);
         //initialising data
         bookmarkedCarparks = new ArrayList<>();
-        bookmarkedCarparks.add(new BookmarkedCarparks("A81"));
-        bookmarkedCarparks.add(new BookmarkedCarparks("NTU North Spine"));
-        bookmarkedCarparks.add(new BookmarkedCarparks("NIE"));
+        bookmarkedCarparks.add(new BookmarkedCarpark("A81"));
+        bookmarkedCarparks.add(new BookmarkedCarpark("NTU North Spine"));
+        bookmarkedCarparks.add(new BookmarkedCarpark("NIE"));
         initRecyclerView();
     }
 
--- app/src/main/java/com/example/carparkwhere/FilesIdkWhereToPutYet/RecyclerAdapter.java
+++ app/src/main/java/com/example/carparkwhere/FilesIdkWhereToPutYet/RecyclerAdapter.java
@@ -10,7 +10,7 @@ import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.carparkwhere.ModelObjects.BookmarkedCarparks;
+import com.example.carparkwhere.ModelObjects.BookmarkedCarpark;
 import com.example.carparkwhere.R;
 
 import java.util.List;
@@ -18,9 +18,9 @@ import java.util.List;
 public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
     private static final String TAG = "RecyclerAdapter";
     int count = 0; //just for tutorial
-    List<BookmarkedCarparks> bookmarkedCarparks;
+    List<BookmarkedCarpark> bookmarkedCarparks;
 
-    public RecyclerAdapter(List<BookmarkedCarparks> bookmarkedCarparks) {
+    public RecyclerAdapter(List<BookmarkedCarpark> bookmarkedCarparks) {
         this.bookmarkedCarparks = bookmarkedCarparks;
     }
 
@@ -30,7 +30,7 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
 
         Log.i(TAG, "onCreateViewHolder: " + count++);
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
-        View view = layoutInflater.inflate(R.layout.bookmarked_carparks, parent, false);
+        View view = layoutInflater.inflate(R.layout.bookmark_carparks, parent, false);
         ViewHolder viewHolder = new ViewHolder(view);
         return viewHolder;
     }
@@ -38,7 +38,7 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         //this changes the text
-        BookmarkedCarparks bookmarks = bookmarkedCarparks.get(position);
+        BookmarkedCarpark bookmarks = bookmarkedCarparks.get(position);
         holder.textView.setText(bookmarks.getCarparkID());
         boolean isExpanded = bookmarkedCarparks.get(position).isExpanded();
         holder.expandableLayout.setVisibility(isExpanded ? View.VISIBLE : View.GONE);
@@ -64,7 +64,7 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
             textView.setOnClickListener(new View.OnClickListener(){
                 @Override
                 public void onClick(View view) {
-                    BookmarkedCarparks bookmark = bookmarkedCarparks.get(getAdapterPosition());
+                    BookmarkedCarpark bookmark = bookmarkedCarparks.get(getAdapterPosition());
                     bookmark.setExpanded(!bookmark.isExpanded());
                     notifyItemChanged(getAdapterPosition());
                 }
