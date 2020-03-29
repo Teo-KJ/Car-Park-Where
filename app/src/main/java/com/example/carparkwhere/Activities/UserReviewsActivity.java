4d8a1491bb7d3e4e16b74ca8e94cf4408f7902f5
sweesenkoh <sweesenworkstation@gmail.com>
2020-03-29 22:43:04 +0800

    integrated user reviews page

--- app/src/main/java/com/example/carparkwhere/Activities/UserReviewsActivity.java
+++ app/src/main/java/com/example/carparkwhere/Activities/UserReviewsActivity.java
@@ -1,15 +1,29 @@
 package com.example.carparkwhere.Activities;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.carparkwhere.DAO.DAOImplementations.CarparkDaoImpl;
+import com.example.carparkwhere.DAO.DAOImplementations.ReviewDaoImpl;
+import com.example.carparkwhere.DAO.DAOImplementations.UserDataDaoFirebaseImpl;
+import com.example.carparkwhere.DAO.DAOInterfaces.CarparkDao;
+import com.example.carparkwhere.DAO.DAOInterfaces.ReviewDao;
+import com.example.carparkwhere.DAO.DAOInterfaces.UserDataDao;
+import com.example.carparkwhere.FilesIdkWhereToPutYet.NetworkCallEventListener;
+import com.example.carparkwhere.FilesIdkWhereToPutYet.UserNotLoggedInException;
 import com.example.carparkwhere.FilesIdkWhereToPutYet.UserReviewAdapter;
+import com.example.carparkwhere.ModelObjects.Carpark;
 import com.example.carparkwhere.ModelObjects.Review;
 import com.example.carparkwhere.R;
 
@@ -20,10 +34,24 @@ public class UserReviewsActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     UserReviewAdapter recyclerAdapter;
     List<Review> userReviews;
+    ReviewDao reviewDaoHelper;
+    UserDataDao userDataDaoHelper;
+    CarparkDao carparkDaoHelper;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_reviews);
+
+        reviewDaoHelper = new ReviewDaoImpl(this);
+        userDataDaoHelper = new UserDataDaoFirebaseImpl();
+        carparkDaoHelper = new CarparkDaoImpl(this);
+
+        ActionBar bar = getSupportActionBar();
+        bar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#111111")));
+        bar.setDisplayHomeAsUpEnabled(true);
+        bar.setTitle("Your Past Reviews");
 
         recyclerView = findViewById(R.id.recyclerView);
         // setting LinearLayout
@@ -35,13 +63,45 @@ public class UserReviewsActivity extends AppCompatActivity {
         // I changed userRating to a float
         userReviews = new ArrayList<>();
         //can retrieve from firebase here
-//        Review first = new Review("jordon@email.com", (float) 5.0,"NTU Northspine", "Brightly lit and clean", "Jordon");
-//        Review second = new Review("jordon@email.com", (float) 4.0,"NIE", "Clean, little lots", "Jordon");
-//        Review third = new Review("jordon@email.com", (float) 1.0,"Hall 7", "Bad, no food, nothing nearby", "Jordon");
+//        Review first = new Review("jordon@email.com", 5.0,"NTU Northspine", "Brightly lit and clean", "Jordon",12);
+//        Review second = new Review("jordon@email.com", 4.0,"NIE", "Clean, little lots", "Jordon",12);
+//        Review third = new Review("jordon@email.com", 1.0,"Hall 7", "Bad, no food, nothing nearby", "Jordon",12);
 //        userReviews.add(first);
 //        userReviews.add(second);
 //        userReviews.add(third);
-        initRecyclerView();
+
+        try{
+            reviewDaoHelper.getCarparkReviewsByUserEmail(userDataDaoHelper.getUserEmail(), new NetworkCallEventListener() {
+                @Override
+                public <T> void onComplete(T networkCallResult, Boolean isSuccessful, String errorMessage) {
+                    if (isSuccessful){
+                        userReviews = (ArrayList<Review>) networkCallResult;
+                        getCarparkName();
+                        initRecyclerView();
+                    }
+                }
+            });
+        }catch (UserNotLoggedInException e){
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private void getCarparkName(){
+        for (Review review:userReviews){
+            carparkDaoHelper.getCarparkDetailsByID(review.getCarparkId(), new NetworkCallEventListener() {
+                @Override
+                public <T> void onComplete(T networkCallResult, Boolean isSuccessful, String errorMessage) {
+                    if (isSuccessful){
+                        Carpark carpark = (Carpark) networkCallResult;
+                        review.setCarparkName(carpark.getCarparkName());
+                        recyclerAdapter.notifyDataSetChanged();
+                    }
+                }
+            });
+        }
+
     }
 
 
@@ -51,7 +111,34 @@ public class UserReviewsActivity extends AppCompatActivity {
         recyclerView.setAdapter(recyclerAdapter);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
 
+        return super.onOptionsItemSelected(item);
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        try{
+            reviewDaoHelper.getCarparkReviewsByUserEmail(userDataDaoHelper.getUserEmail(), new NetworkCallEventListener() {
+                @Override
+                public <T> void onComplete(T networkCallResult, Boolean isSuccessful, String errorMessage) {
+                    if (isSuccessful){
+                        userReviews = (ArrayList<Review>) networkCallResult;
+                        getCarparkName();
+                        initRecyclerView();
+                    }
+                }
+            });
+        }catch (UserNotLoggedInException e){
+            e.printStackTrace();
+        }
+    }
 }
 
