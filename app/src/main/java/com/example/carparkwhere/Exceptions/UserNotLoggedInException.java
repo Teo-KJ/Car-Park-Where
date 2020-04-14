1e70509fff9c56d4c8d63e7800e15a8828606ae6
sweesenkoh <sweesenworkstation@gmail.com>
2020-04-14 12:29:05 +0800

    Done comments for signInActivity

--- app/src/main/java/com/example/carparkwhere/Exceptions/UserNotLoggedInException.java
+++ app/src/main/java/com/example/carparkwhere/Exceptions/UserNotLoggedInException.java
@@ -1,5 +1,9 @@
 package com.example.carparkwhere.Exceptions;
 
+
+/**
+ * This is the exception class to handle exception thrown if user is not logged in
+ */
 public class UserNotLoggedInException extends Exception {
 
     public UserNotLoggedInException(String errorMessage) {
